4404f7cfbb4c94b1006cad5f857ae7d741ef736b
LzCrazy <mizur@sina.com>
2017-04-11 12:23:56 +0800

    环境搭建完成

--- gulpfile.js
+++ gulpfile.js
@@ -15,34 +15,61 @@ gulp.task('lib', function() {
     gulp.src('bower_components/**/*.js')
         .pipe(gulp.dest(app.devPath + 'vendor'))
         .pipe(gulp.dest(app.prdPath + 'vendor'))
+        .pipe($.connect.reload());
 });
 gulp.task('html', function() {
     gulp.src(app.srcPath + '**/*.html')
         .pipe(gulp.dest(app.devPath))
-        .pipe(gulp.dest(app.prdPath));
+        .pipe(gulp.dest(app.prdPath))
+        .pipe($.connect.reload());
 });
 gulp.task('php', function() {
     gulp.src(app.srcPath + 'data/**/*.php')
         .pipe(gulp.dest(app.devPath+'data'))
-        .pipe(gulp.dest(app.prdPath+'data'));
+        .pipe(gulp.dest(app.prdPath+'data'))
+        .pipe($.connect.reload());
 });
 gulp.task('less', function() {
     gulp.src(app.srcPath + 'style/index.less')
         .pipe($.less())
         .pipe(gulp.dest(app.devPath+'css'))
         .pipe($.cssmin())
-        .pipe(gulp.dest(app.prdPath+'css'));
+        .pipe(gulp.dest(app.prdPath+'css'))
+        .pipe($.connect.reload());
 });
 gulp.task('js',function () {
     gulp.src(app.srcPath + 'script/**/*.js')
         .pipe($.concat('index.js'))
         .pipe(gulp.dest(app.devPath + 'js'))
         .pipe($.uglify())
-        .pipe(gulp.dest(app.prdPath + 'js'));
+        .pipe(gulp.dest(app.prdPath + 'js'))
+        .pipe($.connect.reload());
 });
 gulp.task('image',function () {
     gulp.src(app.srcPath + 'image/**/*')
         .pipe(gulp.dest(app.devPath + 'image'))
         .pipe($.imagemin())
-        .pipe(gulp.dest(app.prdPath + 'image'));
+        .pipe(gulp.dest(app.prdPath + 'image'))
+        .pipe($.connect.reload());
 });
+gulp.task('build', ['image', 'js', 'less', 'lib', 'html', 'php']);
+gulp.task('clean',function () {
+    gulp.src([app.devPath, app.prdPath])
+        .pipe($.clean());
+});
+gulp.task('server',['build'],function () {
+    $.connect.server({
+        root: [app.devPath],
+        liverload: true,
+        port: 3000
+    });
+    open('http://localhost:3000');
+
+    gulp.watch('bower_components/**/*', ['lib']);
+    gulp.watch(app.srcPath + '**/*.html', ['html']);
+    gulp.watch(app.srcPath + 'data/**/*.php', ['php']);
+    gulp.watch(app.srcPath + 'style/**/*.less', ['less']);
+    gulp.watch(app.srcPath + 'script/**/*.js', ['js']);
+    gulp.watch(app.srcPath + 'image/**/*', ['image']);
+});
+gulp.task('default', ['server']);
